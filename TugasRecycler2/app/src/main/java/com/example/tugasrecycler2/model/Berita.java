38d167ba2fb117aedd1d23773518413db4450da7
hafizhdiaz <hafizhdiaz@gmail.com>
2019-10-30 22:26:58 +0700

    Tugas 2 Selesai

---
+++ TugasRecycler2/app/src/main/java/com/example/tugasrecycler2/model/Berita.java
@@ -0,0 +1,30 @@
+package com.example.tugasrecycler2.model;
+
+import android.graphics.drawable.Drawable;
+
+public class Berita {
+
+    public String judul;
+    public Drawable image;
+
+    public Berita(String judul, Drawable image) {
+        this.judul = judul;
+        this.image = image;
+    }
+
+    public String getJudul() {
+        return judul;
+    }
+
+    public void setJudul(String judul) {
+        this.judul = judul;
+    }
+
+    public Drawable getImage() {
+        return image;
+    }
+
+    public void setImage(Drawable image) {
+        this.image = image;
+    }
+}
